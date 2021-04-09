41236a1773c0650ab6f17649973d3ab5c56b7477
mariechu <mariechu@g.ucla.edu>
2021-04-08 20:53:07 -0700

    split server main into main, server, and session
    Change-Id: I8f6fa606e68dfdcd4263f64f8b2bde3e460c0e04
    
    #What refactored server_main.cc into several source files
    
    #Why provide abstraction and more readable api with header files
    
    #How separated session and server class into separate src files with their own header files
    
    #Testing
    manually launched server and tested that it is still functioning correctly with netcat
    
    Change-Id: I8f6fa606e68dfdcd4263f64f8b2bde3e460c0e04

---
+++ include/server.h
@@ -0,0 +1,16 @@
+#include "session.h"
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port);
+
+private:
+  void start_accept();
+
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error);
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
