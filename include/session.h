fc1ca1d86a9a435c271ee28953588aba20d5611c
Cindy Zhang <czhang96@g.ucla.edu>
2021-04-11 06:37:20 +0000

    Add HTTP Response that echos the client HTTP request
    Change-Id: I3cfacb4cb10f3d74c9b3ef8ed8f4dddc25ee43ae

--- include/session.h
+++ include/session.h
@@ -11,13 +11,18 @@ public:
 
   void start();
 
+  static const int MAX_RESPONSE_HEADER_SIZE = 100;
+
 private:
+  void read();
+  void write(char *response_buffer);
+
   void handle_read(const boost::system::error_code &error,
                    size_t bytes_transferred);
 
   void handle_write(const boost::system::error_code &error);
 
   tcp::socket socket_;
-  enum { max_length = 1024 };
+  enum { max_length = 10240 };
   char data_[max_length];
 };
--- src/session.cc
+++ src/session.cc
@@ -3,6 +3,10 @@
 tcp::socket &session::socket() { return socket_; }
 
 void session::start() {
+  session::read();
+}
+
+void session::read() {
   socket_.async_read_some(
       boost::asio::buffer(data_, max_length),
       boost::bind(&session::handle_read, this, boost::asio::placeholders::error,
@@ -12,22 +16,31 @@ void session::start() {
 void session::handle_read(const boost::system::error_code &error,
                           size_t bytes_transferred) {
   if (!error) {
-    boost::asio::async_write(socket_,
-                             boost::asio::buffer(data_, bytes_transferred),
-                             boost::bind(&session::handle_write, this,
-                                         boost::asio::placeholders::error));
+    int buf_size = strlen(data_) + MAX_RESPONSE_HEADER_SIZE;
+    char response_buffer[buf_size]; 
+    sprintf(response_buffer,
+            "%s %s\nContent-Type: %s\nContent-Length: %li\n\n%s",
+            "HTTP/1.1",
+            "200 OK",
+            "text/plain",
+            strlen(data_),
+            data_);
+    session::write(response_buffer);
   } else {
     delete this;
-  }
+  } 
+}
+
+void session::write(char *response_buffer) {
+  boost::asio::async_write(socket_,
+                            boost::asio::buffer(response_buffer, strlen(response_buffer)),
+                            boost::bind(&session::handle_write, this,
+                                        boost::asio::placeholders::error));
 }
 
 void session::handle_write(const boost::system::error_code &error) {
   if (!error) {
-    socket_.async_read_some(
-        boost::asio::buffer(data_, max_length),
-        boost::bind(&session::handle_read, this,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
+    session::read();
   } else {
     delete this;
   }
