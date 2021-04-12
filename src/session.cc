fd27cc693150bad71ce880e588a23bae5301b68c
mariechu <mariechu@g.ucla.edu>
2021-04-11 22:25:10 -0700

    Added changes to Dockerfile + server_config
    Change-Id: I69aae5ee8a8feabae131fbed7f2dd5818ad3c3e8
    
    #What added changes to Dockerfile + server config
    
    #Why create docker image to deploy
    
    #How added server binary + config file to deployment container, exposed port and added entrypoint + binary's arguments
    
    #Testing
    docker build -f docker/base.Dockerfile -t aite:base .
    docker build -f docker/Dockerfile -t server .
    docker run -rm -p 80:80 --name init server:latest
    in a separate terminal:
    curl -v localhost:80
    
    Change-Id: I69aae5ee8a8feabae131fbed7f2dd5818ad3c3e8

--- src/session.cc
+++ src/session.cc
@@ -2,9 +2,7 @@
 
 tcp::socket &session::socket() { return socket_; }
 
-void session::start() {
-  session::read();
-}
+void session::start() { session::read(); }
 
 void session::read() {
   socket_.async_read_some(
@@ -17,25 +15,21 @@ void session::handle_read(const boost::system::error_code &error,
                           size_t bytes_transferred) {
   if (!error) {
     int buf_size = strlen(data_) + MAX_RESPONSE_HEADER_SIZE;
-    char response_buffer[buf_size]; 
+    char response_buffer[buf_size];
     sprintf(response_buffer,
-            "%s %s\nContent-Type: %s\nContent-Length: %li\n\n%s",
-            "HTTP/1.1",
-            "200 OK",
-            "text/plain",
-            strlen(data_),
-            data_);
+            "%s %s\nContent-Type: %s\nContent-Length: %li\n\n%s", "HTTP/1.1",
+            "200 OK", "text/plain", strlen(data_), data_);
     session::write(response_buffer);
   } else {
     delete this;
-  } 
+  }
 }
 
 void session::write(char *response_buffer) {
-  boost::asio::async_write(socket_,
-                            boost::asio::buffer(response_buffer, strlen(response_buffer)),
-                            boost::bind(&session::handle_write, this,
-                                        boost::asio::placeholders::error));
+  boost::asio::async_write(
+      socket_, boost::asio::buffer(response_buffer, strlen(response_buffer)),
+      boost::bind(&session::handle_write, this,
+                  boost::asio::placeholders::error));
 }
 
 void session::handle_write(const boost::system::error_code &error) {
