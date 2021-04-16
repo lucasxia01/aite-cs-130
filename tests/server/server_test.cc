f389861ba24c2718bca0542400b8120c15d77adb
mariechu <mariechu@g.ucla.edu>
2021-04-15 22:16:03 -0700

    Set up directories/test files and updated CMakeLists.txt to properly link libraries and create executables to set up for unit tests
    Change-Id: I6385882929f26d6c7948056d2cf80015722fd3a5
    
    #What setup directories and files and updated CMakeLists.txt to properly create executables and link libraries
    
    #Why create separate files for unit testing of different classes
    
    #Testing
    cd build
    cmake ..
    make
    make test
    ---> check that all tests from config_parser_test.cc still pass and dummy tests from session_test.cc and server_test.cc to make sure everything was properly linked
    
    Change-Id: I6385882929f26d6c7948056d2cf80015722fd3a5

---
+++ tests/server/server_test.cc
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "server.h"
+
+TEST(ServerDummyTest, DummyTest) {
+
+  bool success = true;
+
+  EXPECT_TRUE(success);
+}
---
+++ tests/session/session_test.cc
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "session.h"
+
+TEST(SessionDummyTest, DummyTest) {
+
+  bool success = true;
+
+  EXPECT_TRUE(success);
+}
